c118942ade42edfd7c34ee1cbdd7ace3833bf5e4
happy-farah <far20180723@std.psut.edu.jo>
2023-01-01 23:49:41 +0300

    view field

--- app/src/main/java/com/example/hatrick/GameAdapter.kt
+++ app/src/main/java/com/example/hatrick/GameAdapter.kt
@@ -1,58 +1,68 @@
 package com.example.hatrick
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.type.Color
+import com.google.type.Color.Builder
 import com.squareup.picasso.Picasso
 
 
 class GameAdapter(var c: Context, private val gameList : ArrayList<Reservation>, private val card: String) : RecyclerView.Adapter<GameAdapter.MyViewHolder>() {
 
 
+    @RequiresApi(Build.VERSION_CODES.P)
+    @SuppressLint("MissingInflatedId")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.game_item,
             parent,false)
         val viewField = itemView.findViewById<TextView>(R.id.viewField)
         val joinGame = itemView.findViewById<TextView>(R.id.joinGame)
+        val fieldCard = itemView.findViewById<CardView>(R.id.fCard)
 
         if (card=="Football")
         {
-            viewField.setTextColor(android.graphics.Color.parseColor("#009900"))
-            joinGame.setTextColor(android.graphics.Color.parseColor("#009900"))
+            viewField.setBackgroundColor(android.graphics.Color.parseColor("#009900"))
+            joinGame.setBackgroundColor(android.graphics.Color.parseColor("#009900"))
         }
         if (card=="Basketball")
         {
-            viewField.setTextColor(android.graphics.Color.parseColor("#FF5207"))
-            joinGame.setTextColor(android.graphics.Color.parseColor("#FF5207"))
+            viewField.setBackgroundColor(android.graphics.Color.parseColor("#FF5207"))
+            joinGame.setBackgroundColor(android.graphics.Color.parseColor("#FF5207"))
         }
         if (card=="Tennis")
         {
-            viewField.setTextColor(android.graphics.Color.parseColor("#AAEE00"))
-            joinGame.setTextColor(android.graphics.Color.parseColor("#AAEE00"))
+            viewField.setBackgroundColor(android.graphics.Color.parseColor("#AAEE00"))
+            joinGame.setBackgroundColor(android.graphics.Color.parseColor("#AAEE00"))
         }
         if (card=="Handball")
         {
-            viewField.setTextColor(android.graphics.Color.parseColor("#023e7d"))
-            joinGame.setTextColor(android.graphics.Color.parseColor("#023e7d"))
+            viewField.setBackgroundColor(android.graphics.Color.parseColor("#023e7d"))
+            joinGame.setBackgroundColor(android.graphics.Color.parseColor("#023e7d"))
         }
         if (card=="Badminton")
         {
-            viewField.setTextColor(android.graphics.Color.parseColor("#ae2012"))
-            joinGame.setTextColor(android.graphics.Color.parseColor("#ae2012"))
+            viewField.setBackgroundColor(android.graphics.Color.parseColor("#ae2012"))
+            joinGame.setBackgroundColor(android.graphics.Color.parseColor("#ae2012"))
         }
         if (card=="Volleyball")
         {
-            viewField.setTextColor(android.graphics.Color.parseColor("#4361ee"))
-            joinGame.setTextColor(android.graphics.Color.parseColor("#4361ee"))
+            viewField.setBackgroundColor(android.graphics.Color.parseColor("#4361ee"))
+            joinGame.setBackgroundColor(android.graphics.Color.parseColor("#4361ee"))
         }
 
         return MyViewHolder(itemView)
@@ -67,14 +77,19 @@ class GameAdapter(var c: Context, private val gameList : ArrayList<Reservation>,
         holder.fieldName.text = game.fieldName
         holder.noPlayers.text = game.noplayers.toString()
         holder.viewField.setOnClickListener {
-            var phone:String = " "
-            var email:String = " "
-            var nlocation:String = " "
-            var slength:String = " "
-            var swidth:String = " "
-            var scapacity:String = " "
-            var ground:String = " "
-            var services:String = " "
+            var phone:String = ""
+            var email :String = ""
+            var location :String = ""
+            var length :String = ""
+            var width :String = ""
+            var capacity :String = ""
+            var groundtype:String = ""
+            var services :String = ""
+            var image :String = ""
+            var pricePerPerson :String = ""
+            var wholePrice :String = ""
+            //var openingtime :String = ""
+
 
             val fieldName = game.fieldName
             val userId = game.userID
@@ -95,33 +110,39 @@ class GameAdapter(var c: Context, private val gameList : ArrayList<Reservation>,
                     if (it.isSuccessful) {
                         for (doc in it.result!!) {
 
-                            phone= (doc.data.getValue("phoneNumber") as CharSequence).toString()
-                            email = (doc.data.getValue("email") as CharSequence?).toString()
-                            nlocation=(doc.data.getValue("address") as CharSequence?).toString()
-                            slength = doc.data.getValue("length").toString()
-                            swidth = doc.data.getValue("width").toString()
-                            scapacity = doc.data.getValue("capacity").toString()
-                            ground=(doc.data.getValue("groundType") as CharSequence?).toString()
-                            services=(doc.data.getValue("services") as CharSequence?).toString()
-                            sportstypes=(doc.data.getValue("sportType") as CharSequence?)
-                            val swholePrice = doc.data.getValue("wholePrice").toString()
-                            wholePrice.setText(swholePrice as CharSequence?)
-                            val spricePerPerson = doc.data.getValue("pricePerPerson").toString()
-                            pricePerPerson.setText(spricePerPerson as CharSequence?)
+
+                            phone = doc.data.getValue("phoneNumber").toString()
+                            Toast.makeText(c, "$phone", Toast.LENGTH_SHORT).show()
+                            email = doc.data.getValue("email").toString()
+                            location = doc.data.getValue("address").toString()
+                            length = doc.data.getValue("length").toString()
+                            width = doc.data.getValue("width").toString()
+                            capacity = doc.data.getValue("capacity").toString()
+                            groundtype = doc.data.getValue("groundType").toString()
+                            services = doc.data.getValue("services").toString()
+                            image = doc.data.getValue("image").toString()
+                            pricePerPerson = doc.data.getValue("pricePerPerson").toString()
+                            wholePrice = doc.data.getValue("wholePrice").toString()
+                            //val openingtime = field.openingTimes
+
                         }
+                        intent.putExtra("fieldName",fieldName)
+                        intent.putExtra("phoneNumber",phone)
+                        intent.putExtra("email",email)
+                        intent.putExtra("address",location)
+                        intent.putExtra("length",length)
+                        intent.putExtra("width",width)
+                        intent.putExtra("capacity",capacity)
+                        intent.putExtra("wholePrice",wholePrice)
+                        intent.putExtra("pricePerPerson",pricePerPerson)
+                        intent.putExtra("groundType",groundtype)
+                        intent.putExtra("services",services)
+                        intent.putExtra("image", image)
+                        intent.putExtra("fieldID",fieldId)
+                        //intent.putExtra("openingTimes", openingtime)
                     }
                 }
-            intent.putExtra("fieldName",fieldName)
-            intent.putExtra("phoneNumber",phone)
-            intent.putExtra("email",email)
-            intent.putExtra("address",nlocation)
-            intent.putExtra("length",slength)
-            intent.putExtra("width",swidth)
-            intent.putExtra("capacity",scapacity)
-            intent.putExtra("groundType",ground)
-            intent.putExtra("services",services)
-            intent.putExtra("image", image)
-            intent.putExtra("fieldID",fieldId)
+
             c.startActivity(intent)
         }
 //        intent.putExtra("fieldName",fieldName)
